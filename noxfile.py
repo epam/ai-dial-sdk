1c163e48fed5b5fc68a0984249f2f52e937d6f3f
Aleksandr Chikovani <a@justrp.ru>
2023-09-19 03:18:36 -0400

    chore: test ci

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ["tests"]
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
+@nox.session(python=["3.11"])
 def tests(session: nox.Session) -> None:
     """Runs tests"""
     session.run("poetry", "install", external=True)
