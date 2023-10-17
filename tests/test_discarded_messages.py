6e606935b397474833205a09a2aa174b3c340c4a
Oleksii-Klimov <133792808+Oleksii-Klimov@users.noreply.github.com>
2023-10-17 14:19:58 +0100

    feat: add history truncation parameters (#19) (#14)

---
+++ tests/test_discarded_messages.py
@@ -0,0 +1,127 @@
+import json
+from unittest.mock import Mock
+
+import pytest
+from starlette.testclient import TestClient
+
+from aidial_sdk import DIALApp, HTTPException
+from aidial_sdk.chat_completion import ChatCompletion, Request, Response
+
+
+def test_discarded_messages_returned():
+    dial_app = DIALApp()
+    chat_completion = Mock(spec=ChatCompletion)
+
+    async def chat_completion_side_effect(_, res: Response) -> None:
+        with res.create_single_choice():
+            pass
+        res.set_discarded_messages(12)
+
+    chat_completion.chat_completion.side_effect = chat_completion_side_effect
+    dial_app.add_chat_completion("test_app", chat_completion)
+
+    test_app = TestClient(dial_app)
+
+    response = test_app.post(
+        "/openai/deployments/test_app/chat/completions",
+        json={
+            "messages": [{"role": "user", "content": "Test content"}],
+        },
+        headers={"Api-Key": "TEST_API_KEY"},
+    )
+
+    assert response.json()["statistics"]["discarded_messages"] == 12
+
+
+def test_discarded_messages_returned_as_last_chunk_in_stream():
+    dial_app = DIALApp()
+    chat_completion = Mock(spec=ChatCompletion)
+
+    async def chat_completion_side_effect(_, res: Response) -> None:
+        res.set_response_id("test_id")
+        res.set_created(123)
+
+        with res.create_single_choice():
+            pass
+
+        res.set_discarded_messages(12)
+
+    chat_completion.chat_completion.side_effect = chat_completion_side_effect
+    dial_app.add_chat_completion("test_app", chat_completion)
+
+    test_app = TestClient(dial_app)
+
+    response = test_app.post(
+        "/openai/deployments/test_app/chat/completions",
+        json={
+            "messages": [{"role": "user", "content": "Test content"}],
+            "stream": True,
+        },
+        headers={"Api-Key": "TEST_API_KEY"},
+    )
+
+    def parse_chunk(data: str):
+        return json.loads(data[len("data: ") :])
+
+    def identity(data: str):
+        return data
+
+    parsers = [
+        parse_chunk,
+        identity,
+        parse_chunk,
+        identity,
+        identity,
+        identity,
+    ]
+    lines = [*response.iter_lines()]
+
+    assert len(lines) == len(parsers)
+    assert [parser(lines[i]) for i, parser in enumerate(parsers)] == [
+        {
+            "choices": [
+                {
+                    "index": 0,
+                    "finish_reason": None,
+                    "delta": {"role": "assistant"},
+                }
+            ],
+            "usage": None,
+            "id": "test_id",
+            "created": 123,
+            "object": "chat.completion.chunk",
+        },
+        "",
+        {
+            "choices": [{"index": 0, "finish_reason": "stop", "delta": {}}],
+            "usage": None,
+            "statistics": {"discarded_messages": 12},
+            "id": "test_id",
+            "created": 123,
+            "object": "chat.completion.chunk",
+        },
+        "",
+        "data: [DONE]",
+        "",
+    ]
+
+
+def test_discarded_messages_is_set_twice():
+    request = Request(headers={}, api_key="", deployment_id="", messages=[])
+    response = Response(request)
+
+    with response.create_single_choice():
+        pass
+
+    response.set_discarded_messages(1)
+
+    with pytest.raises(HTTPException):
+        response.set_discarded_messages(1)
+
+
+def test_discarded_messages_is_set_before_choice():
+    request = Request(headers={}, api_key="", deployment_id="", messages=[])
+    response = Response(request)
+
+    with pytest.raises(HTTPException):
+        response.set_discarded_messages(1)
