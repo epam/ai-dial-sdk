8a69658247735b21c63a926c82f8d2db6c84f57b
Uladzislau Vishneuski <Vladisavvv@gmail.com>
2023-10-25 17:01:58 +0200

    fix: error generation format (#23) (#25)

---
+++ tests/applications/runtime_broken_application.py
@@ -0,0 +1,16 @@
+from aidial_sdk.chat_completion import ChatCompletion, Request, Response
+from tests.applications.broken_application import raise_exception
+
+
+class RuntimeBrokenApplication(ChatCompletion):
+    async def chat_completion(
+        self, request: Request, response: Response
+    ) -> None:
+        response.set_response_id("test_id")
+        response.set_created(0)
+
+        with response.create_single_choice() as choice:
+            choice.append_content("Test content")
+            await response.aflush()
+
+            raise_exception(request.messages[0].content or "")
