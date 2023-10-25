e6adf995d0bed0975a4564b45c972fdaf2b73e11
Uladzislau Vishneuski <Vladisavvv@gmail.com>
2023-10-25 15:38:45 +0200

    feat: change default log level to warning (#9) (#26)

--- aidial_sdk/utils/log_config.py
+++ aidial_sdk/utils/log_config.py
@@ -22,7 +22,7 @@ class LogConfig(BaseModel):
         },
     }
     loggers = {
-        "aidial_sdk": {"handlers": ["default"]},
+        "aidial_sdk": {"handlers": ["default"], "level": "WARNING"},
         "uvicorn": {
             "handlers": ["default"],
             "propagate": False,
