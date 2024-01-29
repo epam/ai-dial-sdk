45df5914939934269696b07fba26e84b187ed2c4
Anton Dubovik <anton_dubovik@epam.com>
2024-01-29 17:46:03 +0000

    feat: supported File API in image_size example

--- examples/image_size/app/image.py
+++ examples/image_size/app/image.py
@@ -2,6 +2,7 @@ import base64
 from io import BytesIO
 from typing import Tuple
 
+import requests
 from PIL import Image
 
 
@@ -9,3 +10,17 @@ def get_image_base64_size(image_base64) -> Tuple[int, int]:
     image_binary = base64.b64decode(image_base64)
     img = Image.open(BytesIO(image_binary))
     return img.size
+
+
+def bytes_to_base64(bytes: bytes) -> str:
+    return base64.b64encode(bytes).decode()
+
+
+def download_image_as_bytes(url: str) -> bytes:
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+
+def download_image_as_base64(url: str) -> str:
+    return bytes_to_base64(download_image_as_bytes(url))
--- examples/image_size/app/main.py
+++ examples/image_size/app/main.py
@@ -4,13 +4,28 @@ Takes the last message, extract an image from an attachment and
 returns its width and height as text.
 """
 
-import uvicorn
+import os
+from urllib.parse import urlparse
 
 from aidial_sdk import DIALApp
 from aidial_sdk import HTTPException as DIALException
 from aidial_sdk.chat_completion import ChatCompletion, Request, Response
 
-from .image import get_image_base64_size
+from .image import download_image_as_base64, get_image_base64_size
+
+DIAL_URL = os.environ.get("DIAL_URL")
+
+
+# A helper to distinguish relative URLs from absolute ones
+# Relative URLs are treated as URLs to the DIAL File storage
+# Absolute URLs are treated as publicly accessible URLs to external resources
+def is_relative_url(url) -> bool:
+    parsed_url = urlparse(url)
+    return (
+        not parsed_url.scheme
+        and not parsed_url.netloc
+        and not url.startswith("/")
+    )
 
 
 # ChatCompletion is an abstract class for applications and model adapters
@@ -18,25 +33,50 @@ class ImageSizeApplication(ChatCompletion):
     async def chat_completion(self, request: Request, response: Response):
         # Create a single choice
         with response.create_single_choice() as choice:
-            # Get the image from the last message attachments
-            try:
-                image = request.messages[-1].custom_content.attachments[0].data  # type: ignore
-            except Exception:
+            if (
+                request.messages[-1].custom_content is None
+                or request.messages[-1].custom_content.attachments is None
+            ):
                 # Raise an exception if no image was found
                 raise DIALException(
                     message="No image attachment was found in the last message",
                     status_code=422,
                 )
+
+            # Get the image from the last message attachments
+            attachment = request.messages[-1].custom_content.attachments[0]
+
+            # The attachment contains either the image content as base64 string or the image URL
+            if attachment.data is not None:
+                image_data = attachment.data
+            else:
+                image_url = attachment.url
+                assert image_url is not None, "Image URL is not found"
+
+                # Download the image from the URL
+                if is_relative_url(image_url):
+                    if DIAL_URL is None:
+                        raise Exception(
+                            "DIAL_URL environment variable is not set"
+                        )
+                    image_abs_url = f"{DIAL_URL}/v1/{image_url}"
+                else:
+                    image_abs_url = image_url
+
+                image_data = download_image_as_base64(image_abs_url)
+
             # Compute the image size
-            (w, h) = get_image_base64_size(image)
-            # Display the image size
+            (w, h) = get_image_base64_size(image_data)
+
+            # Return the image size
             choice.append_content(f"Size: {w}x{h}px")
 
 
 # DIALApp extends FastAPI to provide a user-friendly interface for routing requests to your applications
-app = DIALApp()
-app.add_chat_completion("image-size", ImageSizeApplication())
+app = DIALApp(
+    dial_url=DIAL_URL,
+    propagation_auth_headers=DIAL_URL is not None,
+    add_healthcheck=True,
+)
 
-# Run built app
-if __name__ == "__main__":
-    uvicorn.run(app, port=5000)
+app.add_chat_completion("image-size", ImageSizeApplication())
