1039e17254a44778045895f30cc1df7a2ffe73cb
Anton Dubovik <anton_dubovik@epam.com>
2024-02-01 15:33:44 +0000

    feat: updated image-size and render-text examples with file api (#68)

---
+++ examples/render_text/README.md
@@ -0,0 +1,25 @@
+## Overview
+
+An example of a simple text-to-image DIAL application.
+
+It takes a text from the last user message attachments and returns back the image with the rasterized text.
+
+The generated image is added as an image attachment to the response message and also as a Markdown image in the response text.
+
+Upon start the Docker image exposes `openai/deployments/render-text/chat/completions` endpoint at port `5000`.
+
+## Configuration
+
+The application returns the image in one of the following formats:
+1. Base64 encoded image
+2. URL to the image stored in the DIAL file storage. `DIAL_URL` environment variable should be set to support image uploading to the storage.
+
+The format of the image attachment is controlled by the user message, which is expected to have the following format: `(base64|url),<text to render>`.
+
+|Variable|Default|Description|
+|---|---|---|
+|DIAL_URL||URL of the core DIAL server. Optional. Used to upload generated images the DIAL file storage|
+
+## Usage
+
+Find how to integrate the application into the DIAL Core and call it using DIAL API in the [cookbook](https://github.com/epam/ai-dial/blob/main/dial-cookbook/examples/how_to_call_text_to_image_applications.ipynb).
--- examples/render_text/app/image.py
+++ examples/render_text/app/image.py
@@ -1,11 +1,13 @@
 import base64
+import os
 import textwrap
 from io import BytesIO
 
+import aiohttp
 from PIL import Image, ImageDraw, ImageFont
 
 
-def text_to_image_base64(text, img_size=(200, 100), font_size=20) -> str:
+def text_to_image_base64(text: str, img_size=(200, 100), font_size=20) -> str:
     img = Image.new("RGB", img_size, color="yellow")
     d = ImageDraw.Draw(img)
 
@@ -25,3 +27,30 @@ def text_to_image_base64(text, img_size=(200, 100), font_size=20) -> str:
     img_base64 = base64.b64encode(img_buffer.getvalue()).decode()
 
     return img_base64
+
+
+async def upload_png_image(
+    dial_url: str, filepath: str, image_base64: str
+) -> str:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{dial_url}/v1/bucket") as response:
+            response.raise_for_status()
+            appdata = (await response.json())["appdata"]
+
+        image_bytes = base64.b64decode(image_base64)
+
+        data = aiohttp.FormData()
+        data.add_field(
+            name="file",
+            content_type="image/png",
+            value=BytesIO(image_bytes),
+            filename=os.path.basename(filepath),
+        )
+
+        async with session.put(
+            f"{dial_url}/v1/files/{appdata}/{filepath}", data=data
+        ) as response:
+            response.raise_for_status()
+            metadata = await response.json()
+
+    return metadata["url"]
