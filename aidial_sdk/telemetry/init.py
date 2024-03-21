ea5c5a2683e8f148db5ec1b6804170f0ed5f80dd
Anton Dubovik <anton_dubovik@epam.com>
2024-03-21 16:30:28 +0000

    feat: supported otlp export for metrics and logs

--- aidial_sdk/telemetry/init.py
+++ aidial_sdk/telemetry/init.py
@@ -1,4 +1,11 @@
+import logging
+
 from fastapi import FastAPI
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import (
+    OTLPMetricExporter,
+)
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
 )
@@ -15,7 +22,12 @@ from opentelemetry.instrumentation.system_metrics import (
 )
 from opentelemetry.instrumentation.urllib import URLLibInstrumentor
 from opentelemetry.metrics import set_meter_provider
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics._internal.export import (
+    PeriodicExportingMetricReader,
+)
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -31,10 +43,12 @@ def init_telemetry(
     config: TelemetryConfig,
 ):
     resource = Resource.create(
-        attributes={SERVICE_NAME: config.service_name}
-        if config.service_name
-        else None
+        attributes=(
+            {SERVICE_NAME: config.service_name} if config.service_name else None
+        )
     )
+
+    log_level = logger.getEffectiveLevel()
 
     if config.tracing is not None:
         tracer_provider = TracerProvider(resource=resource)
@@ -52,21 +66,46 @@ def init_telemetry(
         HTTPXClientInstrumentor().instrument()
 
         if config.tracing.logging:
+            # Setting the root logger format in order to include
+            # tracing information: span_id, trace_id
             LoggingInstrumentor().instrument(
                 set_logging_format=True,
-                log_level=logger.getEffectiveLevel(),
+                log_level=log_level,
             )
 
-    if config.metrics is not None:
-        set_meter_provider(
-            MeterProvider(
-                resource=resource, metric_readers=[PrometheusMetricReader()]
+    if config.logs is not None:
+        # Adding a handler to the root logger which exports the logs to OTLP
+        provider = LoggerProvider(resource=resource)
+
+        if config.logs.otlp_export:
+            provider.add_log_record_processor(
+                BatchLogRecordProcessor(OTLPLogExporter())
             )
+
+        set_logger_provider(provider)
+
+        handler = LoggingHandler(level=log_level)
+        logging.getLogger().addHandler(handler)
+
+    if config.metrics is not None:
+        metric_readers = []
+
+        if config.metrics.prometheus_export:
+            metric_readers.append(PrometheusMetricReader())
+
+        if config.metrics.otlp_export:
+            metric_readers.append(
+                PeriodicExportingMetricReader(OTLPMetricExporter())
+            )
+
+        set_meter_provider(
+            MeterProvider(resource=resource, metric_readers=metric_readers)
         )
 
         SystemMetricsInstrumentor().instrument()
 
-        start_http_server(port=config.metrics.port)
+        if config.metrics.prometheus_export:
+            start_http_server(port=config.metrics.port)
 
     if config.tracing is not None or config.metrics is not None:
         # FastAPI instrumentor reports both metrics and traces
--- aidial_sdk/telemetry/types.py
+++ aidial_sdk/telemetry/types.py
@@ -1,18 +1,40 @@
+import os
 from typing import Optional
 
 from aidial_sdk.pydantic_v1 import BaseModel
 
+OTEL_LOGS_EXPORTER = os.getenv("OTEL_LOGS_EXPORTER", "").split(",")
+OTEL_TRACES_EXPORTER = os.getenv("OTEL_TRACES_EXPORTER", "").split(",")
+OTEL_METRICS_EXPORTER = os.getenv("OTEL_METRICS_EXPORTER", "").split(",")
+OTEL_EXPORTER_PROMETHEUS_PORT = int(
+    os.getenv("OTEL_EXPORTER_PROMETHEUS_PORT", 9464)
+)
+
+
+class LogsConfig(BaseModel):
+    otlp_export: bool = "otlp" in OTEL_LOGS_EXPORTER
+
 
 class TracingConfig(BaseModel):
-    otlp_export: bool = False
+    otlp_export: bool = "otlp" in OTEL_TRACES_EXPORTER
+
+    """Add tracing information to the console logs"""
     logging: bool = False
 
 
 class MetricsConfig(BaseModel):
-    port: int = 9464
+    otlp_export: bool = "otlp" in OTEL_METRICS_EXPORTER
+    prometheus_export: bool = True
+    port: int = OTEL_EXPORTER_PROMETHEUS_PORT
 
 
 class TelemetryConfig(BaseModel):
     service_name: Optional[str] = None
-    tracing: Optional[TracingConfig] = None
-    metrics: Optional[MetricsConfig] = None
+
+    logs: Optional[LogsConfig] = LogsConfig() if OTEL_LOGS_EXPORTER else None
+    tracing: Optional[TracingConfig] = (
+        TracingConfig() if OTEL_TRACES_EXPORTER else None
+    )
+    metrics: Optional[MetricsConfig] = (
+        MetricsConfig() if OTEL_METRICS_EXPORTER else None
+    )
