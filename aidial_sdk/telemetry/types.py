1baee8a22ccb53c8b75367fbcc3076d8b980232f
Anton Dubovik <anton_dubovik@epam.com>
2024-03-21 16:56:20 +0000

    feat: added env var for logging

--- aidial_sdk/telemetry/types.py
+++ aidial_sdk/telemetry/types.py
@@ -3,11 +3,19 @@ from typing import Optional
 
 from aidial_sdk.pydantic_v1 import BaseModel
 
+# Variables borrowed from OpenTelemetry:
+# https://opentelemetry.io/docs/specs/otel/configuration/sdk-environment-variables/
+
 OTEL_LOGS_EXPORTER = os.getenv("OTEL_LOGS_EXPORTER", "").split(",")
 OTEL_TRACES_EXPORTER = os.getenv("OTEL_TRACES_EXPORTER", "").split(",")
 OTEL_METRICS_EXPORTER = os.getenv("OTEL_METRICS_EXPORTER", "").split(",")
 OTEL_EXPORTER_PROMETHEUS_PORT = int(
     os.getenv("OTEL_EXPORTER_PROMETHEUS_PORT", 9464)
+)
+
+# DIAL-specific env vars
+DIAL_TELEMETRY_ADD_TRACES_TO_LOGS = (
+    os.getenv("DIAL_TELEMETRY_ADD_TRACES_TO_LOGS", "false").lower() == "true"
 )
 
 
@@ -18,8 +26,8 @@ class LogsConfig(BaseModel):
 class TracingConfig(BaseModel):
     otlp_export: bool = "otlp" in OTEL_TRACES_EXPORTER
 
-    """Add tracing information to the console logs"""
-    logging: bool = False
+    """Configure logging to include tracing information into a log message"""
+    logging: bool = DIAL_TELEMETRY_ADD_TRACES_TO_LOGS
 
 
 class MetricsConfig(BaseModel):
