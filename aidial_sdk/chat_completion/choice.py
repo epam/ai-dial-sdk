32de8d73dafffc9362428137e1dfa5957af925bd
Anton Dubovik <anton_dubovik@epam.com>
2024-07-08 10:00:03 +0100

    feat: supported attachment argument in add_attachment methods

--- aidial_sdk/chat_completion/choice.py
+++ aidial_sdk/chat_completion/choice.py
@@ -1,7 +1,7 @@
 import json
 from asyncio import Queue
 from types import TracebackType
-from typing import Any, Optional, Type
+from typing import Any, Optional, Type, overload
 
 from aidial_sdk.chat_completion.choice_base import ChoiceBase
 from aidial_sdk.chat_completion.chunks import (
@@ -15,8 +15,10 @@ from aidial_sdk.chat_completion.chunks import (
 from aidial_sdk.chat_completion.enums import FinishReason
 from aidial_sdk.chat_completion.function_call import FunctionCall
 from aidial_sdk.chat_completion.function_tool_call import FunctionToolCall
+from aidial_sdk.chat_completion.request import Attachment
 from aidial_sdk.chat_completion.stage import Stage
 from aidial_sdk.pydantic_v1 import ValidationError
+from aidial_sdk.utils._attachment import create_attachment
 from aidial_sdk.utils.errors import runtime_error
 from aidial_sdk.utils.logging import log_debug
 
@@ -107,6 +109,10 @@ class Choice(ChoiceBase):
         self._last_finish_reason = FinishReason.FUNCTION_CALL
         return function_call
 
+    @overload
+    def add_attachment(self, attachment: Attachment) -> None: ...
+
+    @overload
     def add_attachment(
         self,
         type: Optional[str] = None,
@@ -115,7 +121,9 @@ class Choice(ChoiceBase):
         url: Optional[str] = None,
         reference_url: Optional[str] = None,
         reference_type: Optional[str] = None,
-    ) -> None:
+    ) -> None: ...
+
+    def add_attachment(self, *args, **kwargs) -> None:
         if not self._opened:
             raise runtime_error(
                 "Trying to add attachment to an unopened choice"
@@ -128,12 +136,7 @@ class Choice(ChoiceBase):
             attachment_chunk = AttachmentChunk(
                 choice_index=self._index,
                 attachment_index=self._last_attachment_index,
-                type=type,
-                title=title,
-                data=data,
-                url=url,
-                reference_url=reference_url,
-                reference_type=reference_type,
+                **create_attachment(*args, **kwargs).dict(),
             )
         except ValidationError as e:
             raise runtime_error(e.errors()[0]["msg"])
