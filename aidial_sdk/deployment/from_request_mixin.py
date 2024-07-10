5207525b268db14ab979915eb7ea00e99540a6cd
Anton Dubovik <anton_dubovik@epam.com>
2024-07-10 11:52:01 +0100

    fix: removed deprecation warnings; removed secrets from headers

--- aidial_sdk/deployment/from_request_mixin.py
+++ aidial_sdk/deployment/from_request_mixin.py
@@ -1,10 +1,8 @@
-import warnings
 from abc import ABC, abstractmethod
 from json import JSONDecodeError
 from typing import Any, Mapping, Optional, Type, TypeVar
 
 import fastapi
-from deprecated import deprecated
 
 from aidial_sdk.exceptions import HTTPException as DIALException
 from aidial_sdk.pydantic_v1 import SecretStr, StrictStr, root_validator
@@ -36,36 +34,22 @@ class FromRequestDeploymentMixin(FromRequestMixin):
     headers: Mapping[StrictStr, StrictStr]
 
     @root_validator(pre=True)
-    def check_deprecated_fields(cls, values: dict):
+    def create_secrets(cls, values: dict):
         if "api_key" in values:
-            warnings.warn(
-                "The 'api_key' property is deprecated. "
-                "Use 'api_key_secret' instead.",
-                DeprecationWarning,
-                stacklevel=4,
-            )
             if "api_key_secret" not in values:
                 values["api_key_secret"] = SecretStr(values.pop("api_key"))
 
         if "jwt" in values:
-            warnings.warn(
-                "The 'jwt' property is deprecated. "
-                "Use 'jwt_secret' instead.",
-                DeprecationWarning,
-                stacklevel=4,
-            )
             if "jwt_secret" not in values:
                 values["jwt_secret"] = SecretStr(values.pop("jwt"))
 
         return values
 
     @property
-    @deprecated(reason="Use 'api_key_secret' property instead")
     def api_key(self) -> str:
         return self.api_key_secret.get_secret_value()
 
     @property
-    @deprecated(reason="Use 'jwt_secret' property instead")
     def jwt(self) -> Optional[str]:
         return self.jwt_secret.get_secret_value() if self.jwt_secret else None
 
@@ -79,7 +63,8 @@ class FromRequestDeploymentMixin(FromRequestMixin):
                 message="Invalid path",
             )
 
-        headers = request.headers
+        headers = request.headers.mutablecopy()
+
         api_key = headers.get("Api-Key")
         if api_key is None:
             raise DIALException(
@@ -87,8 +72,10 @@ class FromRequestDeploymentMixin(FromRequestMixin):
                 type="invalid_request_error",
                 message="Api-Key header is required",
             )
+        del headers["Api-Key"]
 
         jwt = headers.get("Authorization")
+        del headers["Authorization"]
 
         return cls(
             **(await _get_request_body(request)),
