e52637f83d4494eed29e67125b23e019979865d0
Anton Dubovik <anton_dubovik@epam.com>
2024-07-16 15:27:18 +0100

    feat: added custom_input for multi-modal embeddings

---
+++ aidial_sdk/embeddings/__init__.py
@@ -0,0 +1,9 @@
+from aidial_sdk.embeddings.base import Embeddings
+from aidial_sdk.embeddings.request import (
+    Attachment,
+    EmbeddingsMultiModalInput,
+    EmbeddingsRequestCustomFields,
+    EmbeddingsType,
+    Request,
+)
+from aidial_sdk.embeddings.response import Embedding, Response, Usage
