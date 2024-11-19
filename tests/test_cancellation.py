4a622621656787eb15893b694e124e9f57886f79
Anton Dubovik <anton_dubovik@epam.com>
2024-11-19 12:12:30 +0000

    feat: added more tests for cancellation logic

--- tests/test_cancellation.py
+++ tests/test_cancellation.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Optional
 
 import pytest
 
@@ -11,25 +12,81 @@ from aidial_sdk.utils.streaming import add_heartbeat
 from tests.utils.constants import DUMMY_FASTAPI_REQUEST
 
 
-def create_chat_completion(status: asyncio.Future):
+class Counter:
+    done: int = 0
+    cancelled: int = 0
+    _lock: asyncio.Lock
 
-    async def _chat_completion(
-        request: ChatCompletionRequest, response: ChatCompletionResponse
-    ):
-        try:
-            for _ in range(10):
-                await asyncio.sleep(1)
-        except asyncio.CancelledError:
-            status.set_result(True)
-            raise
+    def __init__(self) -> None:
+        self._lock = asyncio.Lock()
+
+    async def inc_done(self):
+        async with self._lock:
+            self.done += 1
+
+    async def inc_cancelled(self):
+        async with self._lock:
+            self.cancelled += 1
+
+
+async def _wait_forever():
+    await asyncio.Event().wait()
+
+
+async def _wait(counter: Counter, secs: Optional[int] = None):
+    try:
+        if secs is None:
+            # wait forever
+            await _wait_forever()
         else:
-            status.set_result(False)
+            for _ in range(secs):
+                await asyncio.sleep(1)
+    except asyncio.CancelledError:
+        await counter.inc_cancelled()
+        raise
+    else:
+        await counter.inc_done()
+
+
+def chat_completion_wait_forever(counter: Counter):
+
+    async def _chat_completion(*args, **kwargs):
+        await _wait(counter)
+
+    return _chat_completion
+
+
+def chat_completion_gather(counter: Counter):
+
+    async def _chat_completion(*args, **kwargs):
+        tasks = (asyncio.create_task(_wait(counter)) for _ in range(10))
+        await asyncio.gather(*tasks)
+
+    return _chat_completion
+
+
+def chat_completion_create_task(counter: Counter):
+
+    async def _chat_completion(*args, **kwargs):
+        for _ in range(10):
+            asyncio.create_task(_wait(counter, 3))
+        await _wait_forever()
 
     return _chat_completion
 
 
 @pytest.mark.parametrize("with_heartbeat", [True, False])
-async def test_cancellation(with_heartbeat: bool):
+@pytest.mark.parametrize(
+    "chat_completion, expected_cancelled, expected_done",
+    [
+        (chat_completion_wait_forever, 1, 0),
+        (chat_completion_gather, 10, 0),
+        (chat_completion_create_task, 0, 10),
+    ],
+)
+async def test_cancellation(
+    with_heartbeat: bool, chat_completion, expected_cancelled, expected_done
+):
 
     request = ChatCompletionRequest(
         original_request=DUMMY_FASTAPI_REQUEST,
@@ -41,8 +98,8 @@ async def test_cancellation(with_heartbeat: bool):
 
     response = ChatCompletionResponse(request)
 
-    cancellation_flag: asyncio.Future = asyncio.Future()
-    chat_completion = create_chat_completion(cancellation_flag)
+    counter = Counter()
+    chat_completion = chat_completion(counter)
 
     async def _exhaust_stream(stream):
         async for _ in stream:
@@ -63,4 +120,9 @@ async def test_cancellation(with_heartbeat: bool):
     else:
         assert False, "Stream should have timed out"
 
-    assert await cancellation_flag, "Stream should have been cancelled"
+    await asyncio.sleep(2)
+
+    assert (
+        counter.cancelled == expected_cancelled
+        and counter.done == expected_done
+    ), "Stream should have been cancelled"
