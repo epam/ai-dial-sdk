37c46227d2035bca6b6e0744665e0147c03bbeee
Sergei Zinchenko <sergei.zinchenko@epam.com>
2025-02-26 11:27:41 +0100

    configuration request application properties from headers

--- aidial_sdk/deployment/configuration.py
+++ aidial_sdk/deployment/configuration.py
@@ -1,15 +1,34 @@
-import fastapi
+from json import loads, JSONDecodeError
 
+from fastapi import Request
 from aidial_sdk.deployment.from_request_mixin import FromRequestDeploymentMixin
 from aidial_sdk.pydantic_v1 import BaseModel
-from aidial_sdk.utils.pydantic import ExtraForbidModel
 from typing import Optional, Dict, Any
-
-class ConfigurationRequestCustomFields(ExtraForbidModel):
-    application_properties: Optional[Dict[str, Any]] = None
+from aidial_sdk.exceptions import HTTPException as DIALException
 
 class ConfigurationRequest(FromRequestDeploymentMixin):
-    custom_fields: Optional[ConfigurationRequestCustomFields] = None
+    application_properties: Optional[Dict[str, Any]] = None
+
+    @staticmethod
+    async def get_request_body(request: Request) -> dict:
+        return {}
+
+    @classmethod
+    async def from_request(cls, request: Request, deployment_id: str):
+        inst = await super().from_request(request, deployment_id)
+        props_header = request.headers.get("X-APPLICATION-PROPERTIES")
+        try:
+            if props_header:
+                inst.application_properties = loads(props_header)
+                del inst.headers["X-APPLICATION-PROPERTIES"]
+        except JSONDecodeError as e:
+            raise DIALException(
+                status_code=400,
+                type="invalid_request_error",
+                message=f"The application properties header isn't valid JSON: {e.msg}",
+            )
+        return inst
+
 
 class ConfigurationResponse(BaseModel):
     class Config:
