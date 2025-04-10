e005b36242613ab468c63cafad03cebe1378b23d
Anton Dubovik <anton_dubovik@epam.com>
2025-04-10 10:33:35 +0100

    fix: fix streaming tool calling

--- tests/test_tool_calling.py
+++ tests/test_tool_calling.py
@@ -17,10 +17,16 @@ class ToolCaller(ChatCompletion):
         with response.create_single_choice() as choice:
             choice.append_content("Test content")
 
-            tool_call = choice.create_function_tool_call("id", "test_tool")
-            tool_call.append_arguments('{"key')
-            tool_call.append_arguments('":"')
-            tool_call.append_arguments('val"}')
+            tool_call1 = choice.create_function_tool_call(
+                "tool_call_id1", "tool_name"
+            )
+            tool_call1.append_arguments('{"key')
+            tool_call1.append_arguments('":"')
+            tool_call1.append_arguments('val"}')
+
+            choice.create_function_tool_call(
+                "tool_call_id2", "tool_name", '{"foo":"bar"}'
+            )
 
 
 def test_tool_call_non_streaming():
@@ -31,12 +37,20 @@ def test_tool_call_non_streaming():
     body = response.json()
     assert body["choices"][0]["message"]["tool_calls"] == [
         {
-            "function": {
-                "arguments": '{"key":"val"}',
-                "name": "test_tool",
-            },
-            "id": "id",
+            "id": "tool_call_id1",
             "type": "function",
+            "function": {
+                "name": "tool_name",
+                "arguments": '{"key":"val"}',
+            },
+        },
+        {
+            "id": "tool_call_id2",
+            "type": "function",
+            "function": {
+                "name": "tool_name",
+                "arguments": '{"foo":"bar"}',
+            },
         },
     ]
 
@@ -52,11 +66,18 @@ def test_tool_call_streaming():
             create_single_choice_chunk({"role": "assistant"}),
             create_single_choice_chunk({"content": "Test content"}),
             create_tool_call_chunk(
-                0, type="function", id="id", name="test_tool"
+                0, type="function", id="tool_call_id1", name="tool_name"
             ),
             create_tool_call_chunk(0, arguments='{"key'),
             create_tool_call_chunk(0, arguments='":"'),
             create_tool_call_chunk(0, arguments='val"}'),
-            create_single_choice_chunk({}, "tool_call"),
+            create_tool_call_chunk(
+                1,
+                type="function",
+                id="tool_call_id2",
+                name="tool_name",
+                arguments='{"foo":"bar"}',
+            ),
+            create_single_choice_chunk({}, "tool_calls"),
         ],
     )
