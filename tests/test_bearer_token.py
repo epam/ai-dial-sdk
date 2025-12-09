67027d0e8b44fddd1f62792e67c38ac47c52ca07
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2025-12-09 08:23:19 +0100

    test: fix parameter name in chat_completion method for clarity

--- tests/test_bearer_token.py
+++ tests/test_bearer_token.py
@@ -61,10 +61,10 @@ def test_bearer_token_parsing(authz_header, expected_jwt, expected_bearer):
 
 def test_bearer_token_is_removed_from_headers_forwarding():
     class InspectHeadersApp(TokenEchoApp):
-        async def chat_completion(self, request: Request, response: Response):
+        async def chat_completion(self, request: Request, resp: Response):
             assert "Authorization" not in request.headers
             assert "Api-Key" not in request.headers
-            await super().chat_completion(request, response)
+            await super().chat_completion(request, resp)
 
     client = create_client(InspectHeadersApp())
 
