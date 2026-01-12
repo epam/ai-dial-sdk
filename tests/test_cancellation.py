ddb49f4bf1c464914eb1e276452268eccfab9a3e
Timur Golybovich <timur_golybovich@epam.com>
2026-01-12 13:16:18 +0100

    fixed mr remarks iteration 1

--- tests/test_cancellation.py
+++ tests/test_cancellation.py
@@ -102,9 +102,7 @@ async def test_cancellation(
             pass
 
     try:
-        stream = response._generate_stream(
-            response.request.original_request, chat_completion
-        )
+        stream = response._generate_stream(chat_completion)
         if with_heartbeat:
             stream = add_heartbeat(
                 stream,
