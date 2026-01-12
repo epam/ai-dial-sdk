00b49b7445c919d03180baea5ad871206a8ea104
Timur Golybovich <timur_golybovich@epam.com>
2026-01-12 17:03:18 +0100

    fixed mr remarks iteration 3

--- aidial_sdk/utils/_disconnect_middleware.py
+++ aidial_sdk/utils/_disconnect_middleware.py
@@ -1,75 +1,64 @@
 import asyncio
 from typing import Any, MutableMapping
 
+from starlette.types import ASGIApp, Receive, Scope, Send
+
 from aidial_sdk.utils.logging import log_error
 
-Scope = MutableMapping[str, Any]
 
-
-def log_client_disconnect(scope: Scope) -> None:
-    client = (
-        f"{scope['client'][0]}:{scope['client'][1]}"
-        if scope.get("client")
-        else "-:-"
-    )
+def _log_client_disconnect(scope: Scope) -> None:
+    client = scope.get("client", ("-", "-"))
     method = scope.get("method", "-")
     path = scope.get("path", "-")
-
     log_error(
-        '%s - "%s %s" 499 DISCONNECTED',
-        client,
-        method,
-        path,
+        f'{client[0]}:{client[1]} - "{method} {path}" Client disconnected'
     )
 
 
 class DisconnectMiddleware:
-    def __init__(self, app):
+    def __init__(self, app: ASGIApp):
         self.app = app
 
-    async def __call__(self, scope, receive, send):
+    async def __call__(
+        self, scope: Scope, receive: Receive, send: Send
+    ) -> None:
         if scope["type"] != "http":
             await self.app(scope, receive, send)
             return
 
-        queue = asyncio.Queue()
+        queue: asyncio.Queue = asyncio.Queue()
         response_completed = False
-        response_started = False
 
-        async def message_poller(sentinel, handler_task):
-            nonlocal queue
+        async def message_poller(handler_task: asyncio.Task) -> None:
             while True:
                 message = await receive()
                 if message["type"] == "http.disconnect":
                     # Only cancel if response hasn't completed (premature disconnect)
                     if not response_completed:
                         handler_task.cancel()
-                        return sentinel
+                        return
                     # If response completed, this is normal - don't cancel
-                    return sentinel
+                    return
                 await queue.put(message)
 
-        async def send_wrapper(response):
-            nonlocal response_started, response_completed
-            if response["type"] == "http.response.start":
-                response_started = True
-            elif response["type"] == "http.response.body":
-                if not response.get("more_body", False):
-                    response_completed = True  # Response sent
+        async def send_wrapper(response: MutableMapping[str, Any]) -> None:
+            nonlocal response_completed
+            if response["type"] == "http.response.body" and not response.get(
+                "more_body", False
+            ):
+                response_completed = True
             await send(response)
 
-        sentinel = object()
-        handler_task = asyncio.create_task(
-            self.app(scope, queue.get, send_wrapper)
-        )
-        poller_task = asyncio.create_task(
-            message_poller(sentinel, handler_task)
-        )
+        async def run_app() -> None:
+            await self.app(scope, queue.get, send_wrapper)
+
+        handler_task = asyncio.create_task(run_app())
+        poller_task = asyncio.create_task(message_poller(handler_task))
 
         try:
-            return await handler_task
+            await handler_task
         except asyncio.CancelledError:
-            log_client_disconnect(scope)
+            _log_client_disconnect(scope)
         finally:
             if not poller_task.done():
                 poller_task.cancel()
