61078d07f2540f5c6b8f099554a34a5ec14f7b12
Anton Dubovik <anton_dubovik@epam.com>
2026-01-19 14:22:27 +0000

    chore: minor fix

--- examples/tic_tac_toe/app/__main__.py
+++ examples/tic_tac_toe/app/__main__.py
@@ -1,9 +1,9 @@
+import os
+import uvicorn
+
 # The example creates Pydantic V2 models.
 # In order to make them compatible with the models used in the DIAL SDK
 # we need to enable the compatibility mode with Pydantic V2 via the env variable:
-import os
-
-import uvicorn
 os.environ["PYDANTIC_V2"] = "True"
 
 # Run built app
