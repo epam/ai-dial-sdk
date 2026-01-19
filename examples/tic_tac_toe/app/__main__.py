5aebd641f02fc9b84cf6f4a8c116da3e302bda36
Anton Dubovik <anton_dubovik@epam.com>
2026-01-19 14:20:12 +0000

    chore: fix the tic-tac-toe example

---
+++ examples/tic_tac_toe/app/__main__.py
@@ -0,0 +1,12 @@
+# The example creates Pydantic V2 models.
+# In order to make them compatible with the models used in the DIAL SDK
+# we need to enable the compatibility mode with Pydantic V2 via the env variable:
+import os
+
+import uvicorn
+os.environ["PYDANTIC_V2"] = "True"
+
+# Run built app
+if __name__ == "__main__":
+    from app.main import app
+    uvicorn.run(app, port=5000)
