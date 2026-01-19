e598f771a3aabd60b824a53526263ee8ca55f075
Anton Dubovik <anton_dubovik@epam.com>
2026-01-19 14:39:36 +0000

    chore: fix example test

--- examples/tic_tac_toe/app/__main__.py
+++ examples/tic_tac_toe/app/__main__.py
@@ -1,11 +1,10 @@
 import os
+
 import uvicorn
 
-# The example creates Pydantic V2 models.
-# In order to make them compatible with the models used in the DIAL SDK
-# we need to enable the compatibility mode with Pydantic V2 via the env variable:
 os.environ["PYDANTIC_V2"] = "True"
 
 if __name__ == "__main__":
-    from app.main import app
+    from .main import app
+
     uvicorn.run(app, port=5000)
