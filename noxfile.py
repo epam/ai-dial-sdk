9e7789fae9325cb9b38e0556af4878dda1d0917c
Anton Dubovik <anton_dubovik@epam.com>
2026-07-30 12:33:20 +0100

    chore: simplify noxfile

--- noxfile.py
+++ noxfile.py
@@ -42,18 +42,20 @@ class UsePydanticV2(Enum):
     NO = "0"
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
 # Testing against earliest and latest supported versions of the dependencies
-@nox.parametrize(
-    "pydantic",
-    [
-        ("1.10.17", UsePydanticV2.NO),
-        ("2.8.2", UsePydanticV2.NO),
-        ("2.8.2", UsePydanticV2.YES),
-        ("2.13.1", UsePydanticV2.NO),
-        ("2.13.1", UsePydanticV2.YES),
-    ],
-)
+_PYTHONS = ["3.10", "3.11", "3.12", "3.13"]
+
+_PYDANTICS = [
+    ("1.10.17", UsePydanticV2.NO),
+    ("2.8.2", UsePydanticV2.NO),
+    ("2.8.2", UsePydanticV2.YES),
+    ("2.13.1", UsePydanticV2.NO),
+    ("2.13.1", UsePydanticV2.YES),
+]
+
+
+@nox.session(python=_PYTHONS)
+@nox.parametrize("pydantic", _PYDANTICS)
 @nox.parametrize("httpx", ["0.25.0", "0.27.0"])
 def test(
     session: nox.Session, pydantic: tuple[str, UsePydanticV2], httpx: str
@@ -95,17 +97,8 @@ _OTEL_INSTRUMENTATIONS = [
 _OTEL_CORE = ["sdk", "api", "exporter-otlp-proto-grpc"]
 
 
-@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
-@nox.parametrize(
-    "pydantic",
-    [
-        ("1.10.17", UsePydanticV2.NO),
-        ("2.8.2", UsePydanticV2.NO),
-        ("2.8.2", UsePydanticV2.YES),
-        ("2.13.1", UsePydanticV2.NO),
-        ("2.13.1", UsePydanticV2.YES),
-    ],
-)
+@nox.session(python=_PYTHONS)
+@nox.parametrize("pydantic", _PYDANTICS)
 @nox.parametrize("otel", list(_OTEL_CORE_VERSION))
 def test_telemetry(
     session: nox.Session, pydantic: tuple[str, UsePydanticV2], otel: str
